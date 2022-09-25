e3a73e6c5cf33f6fd8bfbc3825715466e59e4722
Minh Nguyen <minh.nguyen@imc.com>
2022-09-25 20:03:47 +0200

    Add stock market date selection to main script

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import sys
 import questionary as q
+import pandas as pd
+import pandas_market_calendars as mcal
+
+
+def get_analysis_date():
+    time_format = "%Y-%m-%d"
+    nyse_cal = mcal.get_calendar('NYSE')
+
+    today = pd.Timestamp.today()
+    start_date = (today - pd.Timedelta(730, "d")).strftime(time_format)
+    end_date = today.strftime(time_format)
+
+    valid_nyse_days = nyse_cal.valid_days(start_date=start_date, end_date=end_date)
+
+    analysis_date = q.autocomplete("Pick a stock market date (yyyy-MM-dd format) within the last 2 years for analysis:",
+                                   choices=[d.strftime(time_format) for d in valid_nyse_days]).ask()
+
+    return analysis_date
 
 
 def user_input():
@@ -15,10 +33,11 @@ def user_input():
         print("Ok! Exiting script...")
         sys.exit(0)
 
-    return twitter_user, stock_ticker
+    analysis_date = get_analysis_date()
+
+    return twitter_user, stock_ticker, analysis_date
 
 
 if __name__ == '__main__':
-    twitter_user, stock_ticker = user_input()
-    print(f"Analyzing twitter sentiment of @{twitter_user} against stock price of ^{stock_ticker}...")
-
+    twitter_user, stock_ticker, analysis_date = user_input()
+    print(f"Analyzing twitter sentiment of @{twitter_user} against stock price of ^{stock_ticker} on {analysis_date}...")
