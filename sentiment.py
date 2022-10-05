6093fa154d743dd1e413ffa4c2325dc773bc2607
Minh Nguyen <minh.nguyen@imc.com>
2022-10-04 20:53:50 -0500

    Sentiment: added output file check, condensed pipeline, updated weighted sentiment calculation

--- sentiment.py
+++ sentiment.py
@@ -1,18 +1,20 @@
 import asent
-import hvplot as hv
-import numpy as np
 import pandas as pd
 import spacy
 from datetime import datetime
-from dframcy import dframcy
+from pathlib import Path
 from spacytextblob.spacytextblob import SpacyTextBlob
 
 
-def get_tweet_sentiment(twitter_user, analysis_date):
+def get_tweet_sentiment(twitter_user, analysis_date, custom_tweets_file=None):
     print("Gathering sentiment from your selection...")
 
-    tweets_file_csv = f"{twitter_user}-{analysis_date}.csv"
+    tweets_file_csv = f"{twitter_user}-{analysis_date}.csv" if custom_tweets_file is None else custom_tweets_file
     sentiment_file_csv = f"{twitter_user}-{analysis_date}-sentiment.csv"
+
+    if Path(sentiment_file_csv).is_file():
+        print(f"Skipping sentiment analysis - file {sentiment_file_csv} exists")
+        return
 
     try:
         f = open(tweets_file_csv)
@@ -27,23 +29,19 @@ def get_tweet_sentiment(twitter_user, analysis_date):
 
     # Adding spacy model and the sentencizer and asent pipelines
 
-    nlp = spacy.blank('en')
+    nlp = spacy.load('en_core_web_lg')
     nlp.add_pipe('sentencizer')
     nlp.add_pipe('asent_en_v1')
+    nlp.add_pipe('spacytextblob')
 
     tweet_df["sentiment_asent"] = tweet_df["tweet"].apply(lambda tweet: nlp(tweet)._.polarity.compound)
-
-    nlp2 = spacy.load('en_core_web_sm')
-    nlp2.add_pipe('spacytextblob')
-    nlp2
-
-    tweet_df['sentiment_textblob'] = tweet_df['tweet'].apply(lambda tweet: nlp2(tweet)._.blob.polarity)
-
+    tweet_df['sentiment_textblob'] = tweet_df['tweet'].apply(lambda tweet: nlp(tweet)._.blob.polarity)
     tweet_df['average_sentiment'] = tweet_df[["sentiment_asent", "sentiment_textblob"]].mean(axis=1)
 
-    # weighted score = followers * sentiment score + followers * sentiment score / total number of followers
+    # tweet_weighted_sentiment = followers * average_sentiment
 
-    tweet_df['weighted_sentiment'] = (tweet_df["followers"])*(tweet_df["average_sentiment"]).sum()/tweet_df["followers"].sum()
+    tweet_df['weighted_sentiment'] = tweet_df["average_sentiment"] * tweet_df["followers"]
+    tweet_df.to_csv(sentiment_file_csv, index = True)
 
-    output_file = tweet_df.to_csv(sentiment_file_csv, index = True)
-    print('\nCSV String:\n', output_file)
+if __name__ == '__main__':
+    get_tweet_sentiment("elonmusk", "2021-09-25", custom_tweets_file="Files/demo_tweets.csv")
