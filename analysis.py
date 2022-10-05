f4d2255d6a1876aa763d51fa3a061cc9e51ca4e5
Minh Nguyen <minh.nguyen@imc.com>
2022-10-04 22:12:19 -0500

    Analysis: read proper stock and sentiment data, save plot to html file

--- analysis.py
+++ analysis.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
+
 def generate_dummy_sentiments(stock_change_df):
     sentiment_df = stock_change_df.copy()
     num_rows = sentiment_df.shape[0]
@@ -20,23 +21,34 @@ def generate_dummy_sentiments(stock_change_df):
 
 
 def perform_analysis(twitter_handle, stock_ticker, analysis_date, dummy_sentiments=False):
-    stock_csv = f"{stock_ticker}-{analysis_date}.csv"
-    sentiment_csv = f"{twitter_handle}-{analysis_date}-sentiment.csv"
+    print("Performing correlation analysis...")
+    stock_csv = f"^{stock_ticker}-{analysis_date}.csv"
+    sentiment_csv = f"@{twitter_handle}-{analysis_date}-sentiment.csv"
 
-    stock_df = pd.read_csv(stock_csv, parse_dates=True, infer_datetime_format=True, index_col='Date')
-    stock_change_df = stock_df.pct_change().dropna()
+    stock_df = pd.read_csv(stock_csv, parse_dates=True, infer_datetime_format=True)
+    stock_df.drop(columns='Unnamed: 0', inplace=True)
+    stock_df['Date'] = pd.to_datetime(stock_df['Date'], infer_datetime_format=True)
+    stock_df['Change'] = stock_df.Close.pct_change()
+    stock_df.dropna(inplace=True)
+    stock_df.set_index('Date', inplace=True)
+    print(stock_df)
 
     if dummy_sentiments:
         sentiment_df = generate_dummy_sentiments(stock_change_df)
     else:
-        sentiment_df = pd.read_csv(sentiment_csv, parse_dates=True, infer_datetime_format=True, index_col='Date')
+        sentiment_df = pd.read_csv(sentiment_csv, parse_dates=True, infer_datetime_format=True)
 
-    df = pd.concat([stock_change_df, sentiment_df], axis=1)
-    print(df)
-    print(df.corr())
+    aggregated_sentiment_df = sentiment_df[['Date', 'weighted_sentiment', 'followers']].groupby('Date').agg(['sum', 'count'])
+    aggregated_sentiment_df['final_sentiment'] = aggregated_sentiment_df['weighted_sentiment']['sum'] / aggregated_sentiment_df['followers']['sum']
+    aggregated_sentiment_df['final_count'] = aggregated_sentiment_df['weighted_sentiment']['count']
+    print(aggregated_sentiment_df)
 
-    p = df.hvplot.line()
-    hvplot.show(p)
+    stock_plot = stock_df.Change.hvplot.line()
+    sentiment_plot = aggregated_sentiment_df.final_sentiment.hvplot.line()
+
+    stock_plot_png = f"@{twitter_handle}-^{stock_ticker}-{analysis_date}.html"
+
+    hvplot.save(stock_plot + sentiment_plot, stock_plot_png)
 
 if __name__ == "__main__":
-    perform_analysis("elonmusk", "TSLA", dummy_sentiments=True)
+    perform_analysis("elonmusk", "TSLA", "2022-09-27", dummy_sentiments=False)
